99c6af2a65ba4256982de53b3637b8a4643d91b4
LisaYllander92 <Lisaylander92@gmail.com>
2026-05-06 11:36:11 +0200

    ready to deploy

--- streamlit/sthlm_puls/src/sthlm_puls/pages/raw_data.py
+++ streamlit/sthlm_puls/src/sthlm_puls/pages/raw_data.py
@@ -2,7 +2,8 @@ import streamlit as st
 from sthlm_puls.utils.helpers import get_events_df
 
 def raw_data():
-    st.markdown("# RAW DATA")
+    st.title("RAW DATA")
+    st.markdown("Explore the raw dataset")
     st.dataframe(get_events_df())
 
 if __name__ == "__main__":
